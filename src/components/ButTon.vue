46d8dde29be1acd45e41498083db07880ead669b
drakligh2818 <ddavronjon123@gmail.com>
2022-09-13 12:25:33 +0500

    navbar yarattik

---
+++ src/components/ButTon.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="bg-green-400 hover:bg-green-500 duration-300 font-sm text-white rounded py-2 px-16">
+        <slot></slot>
+    </button> 
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
+        <div class="flex items-center">
+            <span class="text-green-500 text-xl mr-1 cursor-pointer">
+                <i class="fa-brands fa-facebook-messenger"></i>
+            </span>
+            <h1 class="text-xl">Designer</h1>
+        </div>
+
+        <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-3xl mt-1">
+            <i :class="[open ? 'fa-sharp fa-solid fa-xmark' : 'fa-sharp fa-solid fa-bars']"></i>
+        </span>
+
+        <ul class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in" :class="[open ? 'left-0' : 'left-[100%]']">
+            <li class="md:mx-4 md:my-0 my-6" v-for="link in links" :key="link">
+                <a :href="link.link" class="text-xl hover:text-green-500">{{ link.name }}</a>
+            </li>
+            
+            <ButTon>Get Started</ButTon>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+import ButTon from "@/components/ButTon.vue"
+import { ref } from '@vue/reactivity'
+export default {
+    components: {
+        ButTon
+    },
+    setup() {
+        let open = ref(false)
+        let links = [
+            {name : "Home", link : "#"},
+            {name : "About", link : "#"},
+            {name : "Help", link : "#"}
+        ]
+
+        function MenuOpen() {
+            open.value = !open.value
+        }
+
+        return{links, open, MenuOpen}
+    }
+}
+</script>
